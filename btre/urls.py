ae711fd73407c05f2f3b81b4fc60c1ca3184353c
Kgosi Motlogelwa <komotlogelwa@gmail.com>
2021-03-01 11:27:38 +0200

    Previous commit didnt go through

--- btre/urls.py
+++ btre/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('listings/', include('listings.urls')),
     #path('accounts/', include('accounts.urls')),
     #path('contacts/', include('contacts.urls')),
-    path('Kobe_Admin/', admin.site.urls) 
+    path('Jan_Admin/', admin.site.urls) 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
