523ed800b96e6f7d497d9e3753cb2fe8d7f8531f
Kgosi Motlogelwa <komotlogelwa@gmail.com>
2021-03-03 08:59:12 +0200

    Changed Debug to False and added , logging=False

--- btre/settings.py
+++ btre/settings.py
@@ -213,20 +213,4 @@ DATABASES['default'].update(db_from_env)
 
 # Configure Django App for Heroku.
 import django_heroku
-django_heroku.settings(locals())
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-        },
-    },
-}
+django_heroku.settings(locals(), logging=False)
