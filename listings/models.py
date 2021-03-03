58c4cbc38f9d10d997780b0d0f8105e83c51ca5b
Kgosi Motlogelwa <komotlogelwa@gmail.com>
2021-03-03 17:54:42 +0200

    Added extra fields for Palapye, Gaborone and FTown

--- listings/models.py
+++ listings/models.py
@@ -27,7 +27,7 @@ class Listing(models.Model):
         ('Petrol','Petrol'),('Diesel','Diesel'),
     ]
     Dealership = [
-        ('Palapye','Palapye')
+        ('Gaborone','Gaborone'),('Palapye','Palapye'),('Francistown','Francistown')
     ]
     Body_Type = [
         ('Compact','Compact'),('Hatchback','Hatchback'),('Sedan','Sedan'),('SUV','SUV'),
