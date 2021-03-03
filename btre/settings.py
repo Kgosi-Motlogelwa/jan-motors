73f6aea96a5c5ca6f23a270c38d5c4f1393bfaf8
Kgosi Motlogelwa <komotlogelwa@gmail.com>
2021-03-03 08:56:54 +0200

    changed carousel

--- btre/settings.py
+++ btre/settings.py
@@ -214,3 +214,19 @@ DATABASES['default'].update(db_from_env)
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
